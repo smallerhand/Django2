b2347fac0b889e6146db26ce389de4a23b37eebd
skim <sam.kim@tobii.co.jp>
2020-05-18 16:56:41 +0900

    update

--- empapp/models.py
+++ empapp/models.py
@@ -6,42 +6,50 @@ class employeeDetail(models.Model):
     fname	=	models.CharField(max_length=30)
     elname	=	models.CharField(max_length=30)
     efname	=	models.CharField(max_length=30)
-    sex_choices = (
-            ('女', 'f',),
-            ('男', 'm',),
-        )
+    f = 'f'
+    m = 'm'
+    SEX = [
+        (f, '女'),
+        (m, '男'),
+    ]
+
     sex	=	models.CharField(
             max_length=1,
-            choices=sex_choices,
+            choices=SEX,
         )
     email	=	models.EmailField(max_length=254)
     birth	=	models.CharField(max_length=30)
     age	=	models.PositiveSmallIntegerField()
     bloodtype_choices = (
-            ('A', 'A',),
-            ('B', 'B',),
-            ('AB', 'AB',),
-            ('O', 'O',),
+        ('A', 'A',),
+        ('B', 'B',),
+        ('AB', 'AB',),
+        ('O', 'O',),
         )
     bloodtype	=	models.CharField(
             max_length=2,
             choices=bloodtype_choices,
         )
-    dep_choices = (
-            ('営業', 'sales',),
-            ('広報', 'pr',),
-            ('開発', 'dev',),
-        )
+    sales = 'sales'
+    pr = 'pr'
+    dev = 'dev'
+    DEP = [
+        (sales, '営業'),
+        (pr, '広報'),
+        (dev, '開発'),
+    ]
     dep	=	models.CharField(
-            max_length=2,
-            choices=dep_choices,
+            max_length=5,
+            choices=DEP,
         )
-    job_choices = (
-            ('一般', 'regular',),
-            ('組織長', 'administrator',),
-        )
+    reg = 'regular'
+    admin = 'administrator'
+    JOB = (
+        (reg, '一般'),
+        (admin, '組織長'),
+    )
     job	=	models.CharField(
-            max_length=3,
-            choices=job_choices,
+            max_length=13,
+            choices=JOB,
         )
     pw	=	models.CharField(max_length=30)
